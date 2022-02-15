b0e423abe3ed4e98bee739dd59d516c82ec3d343
vbarnaaa <60694401+vbarnaaa@users.noreply.github.com>
2022-02-15 14:22:58 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="p-4">
+      <h5 class="text-white h4">Collapsed content</h5>
+      <span class="text-muted">Toggleable via the navbar brand.</span>
+    </div>
+  </div>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <button class="navbar-toggler d-xl-none d-xl-block" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </nav>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.navbar{
+  height: 60px;
+  border-radius: 0 0 10px 10px;
+  background-color: #FFFFFF;
+  box-shadow: 0px 0px 20px rgba(50, 62, 200, 0.1);
+  border-radius: 0px 0px 16px 16px;
+}
+</style>
